677136ce3ee7c10b87d5ac2b6bc237b663ca2e64
Irenity <lilithkard77@gmail.com>
2025-01-23 13:14:27 +0600

    changed text color of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,25 +9,25 @@ const Navbar = async () => {
     const isUserAuthenticated = await isAuthenticated();
 
     return (
-        <section className="flex container mx-auto my-10 border-b-2 pb-6 border-zinc-900">
+        <section className="flex  container mx-auto my-10 border-b-2 pb-6 border-zinc-900">
             {/* text */}
             <div className="w-3/4">
             <h1 className="text-2xl font-bold">Silent Pen</h1>
             <h2 className="text-lg">A Personal Archive ✒️</h2>
             <br />
-            <div className="space-x-3">
-                <Link className="px-3 py-2 rounded-md bg-zinc-900" href={'/'}>Blog</Link>
-                <Link className="px-3 py-2 rounded-md bg-zinc-900" href={'/profile'}>Profile</Link>
+            <div className="space-x-3 text-white">
+                <Link className="px-3 text-white py-2 rounded-md bg-zinc-900" href={'/'}>Blog</Link>
+                <Link className="px-3 text-white py-2 rounded-md bg-zinc-900" href={'/profile'}>Profile</Link>
                 {isUserAuthenticated ? (
-                        <LogoutLink className="px-3 py-2 rounded-md bg-zinc-900">
+                        <LogoutLink className="px-3 py-2 text-white rounded-md bg-zinc-900">
                             Logout
                         </LogoutLink>
                     ) : (
                         <>
-                            <LoginLink className="px-3 py-2 rounded-md bg-zinc-900">
+                            <LoginLink className="px-3 text-white py-2 rounded-md bg-zinc-900">
                                 Sign in
                             </LoginLink>
-                            <RegisterLink className="px-3 py-2 rounded-md bg-zinc-900">
+                            <RegisterLink className="px-3 text-white py-2 rounded-md bg-zinc-900">
                                 Sign up
                             </RegisterLink>
                         </>
